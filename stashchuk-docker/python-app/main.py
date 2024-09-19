10ccbefd271e64472614ae0d86e09d1e70e87dfb
Viktor <palchinskij19@gmail.com>
2024-09-19 14:33:13 +0300

    19.09.24  - python app with Dockerfile

---
+++ stashchuk-docker/python-app/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Welcome to calendar \n')
+
+year= int(input('Type a year: '))
+month = int(input('Type a favorite month: '))
+
+print(calendar.month(year, month))
+
+print('It`s ok')
