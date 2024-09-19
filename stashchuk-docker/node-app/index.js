9144fbf3cadcbbcad7caf68fb3231201c4b3d4b9
Viktor <palchinskij19@gmail.com>
2024-09-19 15:44:19 +0300

    19.09.24  - creating two services with Docker Compose

--- stashchuk-docker/node-app/index.js
+++ stashchuk-docker/node-app/index.js
@@ -1,6 +1,8 @@
 import fs from 'node:fs';
 
-fs.appendFile('my-file', 'File was created by Node.js', (err) => {
-    if(err) throw err;  
+fs.appendFile('my-file.txt', 'File was created by Node.js', (err) => {
+    if (err) throw err;
     console.log('File saved');
-})
+});
+
+setTimeout(() => console.log('Over'), 30000);
